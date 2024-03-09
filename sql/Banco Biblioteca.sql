54c41fe7c0b1dea895ddb930960c373d2ad5f78f
Gabriel Luiz Kasper <glkkasper@gmail.com>
2024-03-08 21:01:59 -0300

    Script SQL e GITIGNORE adicionados

---
+++ sql/Banco Biblioteca.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE biblioteca;
+USE biblioteca;
+
+CREATE TABLE livros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    ano_publicacao INT
+);
+INSERT INTO livros (titulo, autor, ano_publicacao) VALUES ('Biblia', 'Deus', 1000);
+INSERT INTO livros (titulo, autor, ano_publicacao) VALUES ('Caderno', 'Gabriel', 2023);
+
+SELECT * FROM livros;
+SELECT titulo, autor, ano_publicacao FROM livros WHERE id = 1;
+
+UPDATE livros SET autor = 'Osvaldo' WHERE id = 2;
+
+DELETE FROM livros WHERE id = 2;
