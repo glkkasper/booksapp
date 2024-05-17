17b46bb184da7759a6ed7b5801d8a171304dbb01
Osvaldo Melo <omelo@redhat.com>
2024-05-17 20:58:43 -0300

    Back end criado

---
+++ books-app-backend/src/main/java/org/acme/Book.java
@@ -0,0 +1,20 @@
+package org.acme;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "livros")
+public class Book extends PanacheEntity {
+    
+    @Column(name = "titulo")
+    public String titulo;
+    
+    @Column(name = "autor")
+    public String autor;
+    
+    @Column(name = "ano_publicacao")
+    public int anoPublicacao;
+}
