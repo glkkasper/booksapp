fab4bbb9977d2a7c82651f0ddb7382e318af5a3e
glkkasper <glkkasper@gmail.com>
2024-07-12 20:39:42 -0300

    adicionado tabela autores

--- sql/BancoBiblioteca.sql
+++ sql/BancoBiblioteca.sql
@@ -1,18 +1,35 @@
 CREATE DATABASE biblioteca;
 USE biblioteca;
 
+
+CREATE TABLE autores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL
+);
 CREATE TABLE livros (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
-    autor VARCHAR(255) NOT NULL,
     ano_publicacao INT,
-    numero_de_paginas INT
+    autor_id INT,
+    numero_de_paginas INT,
+    FOREIGN KEY (autor_id) REFERENCES autores(id)
 );
-INSERT INTO livros (titulo, autor, ano_publicacao, numero_de_paginas) VALUES ('Biblia', 'Deus', 1000, 150);
-INSERT INTO livros (titulo, autor, ano_publicacao, numero_de_paginas) VALUES ('Caderno', 'Gabriel', 2023, 200);
+
+INSERT INTO autores (nome, data_nascimento) VALUES ('Deus', '0000-01-01');
+INSERT INTO autores (nome, data_nascimento) VALUES ('Gabriel', '1990-06-15');
+
+INSERT INTO livros (titulo, ano_publicacao, numero_de_paginas, autor_id) VALUES ('Biblia', 1000, 1950, 1);
+INSERT INTO livros (titulo, ano_publicacao, numero_de_paginas, autor_id) VALUES ('Caderno', 2023, 3000, 2);
 
 SELECT * FROM livros;
-SELECT titulo, autor, ano_publicacao FROM livros WHERE id = 1;
+SELECT * FROM autores;
+
+--exemplo com JOIN
+SELECT livros.id,livros.titulo,livros.ano_publicacao,livros.numero_de_paginas as 'Número de Paginas', autores.nome as autor FROM livros 
+ JOIN autores ON livros.autor_id = autores.id 
+ WHERE titulo = 'Biblia';
+ --exemplo com JOIN
 
 /*UPDATE livros SET autor = 'Osvaldo' WHERE id = 2;*/
 
