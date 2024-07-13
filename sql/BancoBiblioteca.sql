883711bac81d45c634e1bc5ad0423b35032f39c4
glkkasper <glkkasper@gmail.com>
2024-07-12 22:34:48 -0300

    Adicionado tabela autores

--- sql/BancoBiblioteca.sql
+++ sql/BancoBiblioteca.sql
@@ -26,11 +26,8 @@ SELECT * FROM livros;
 SELECT * FROM autores;
 
 --exemplo com JOIN
-SELECT livros.id,livros.titulo,livros.ano_publicacao,livros.numero_de_paginas as 'Número de Paginas', autores.nome as autor FROM livros 
- JOIN autores ON livros.autor_id = autores.id 
+SELECT livros.id,livros.titulo,livros.ano_publicacao,livros.numero_de_paginas as 'Número de Paginas', autores.nome as autor FROM livros JOIN autores ON livros.autor_id = autores.id 
  WHERE titulo = 'Biblia';
- --exemplo com JOIN
-
 /*UPDATE livros SET autor = 'Osvaldo' WHERE id = 2;*/
 
 /*DELETE FROM livros WHERE id = 2;*/
